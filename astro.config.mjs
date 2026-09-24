72c3ecf2e069d2ded5f6fa358c4e260530ab50a9
Muhriz Ali <muhriz.ali.scholar@gmail.com>
2026-09-24 23:27:12 +0530

    changed footer, added more font weights, tested with more fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,11 +26,13 @@ export default defineConfig({
       provider: fontProviders.fontsource(),
       name: "IBM Plex Mono",
       cssVariable: "--font-plex-mono",
+      weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
     },
     {
       provider: fontProviders.fontsource(),
       name: "Fira Code",
       cssVariable: "--font-fira-code",
+      // weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
     },
   ],
 
